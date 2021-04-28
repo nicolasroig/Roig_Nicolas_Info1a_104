a8a1c715723a8c7ac8dc4c6c91d967b1ec162e36
nicolasroig <nicolas.roigr@gmail.com>
2021-04-28 09:59:36 +0200

    Add, change, delete

---
+++ 2021_mod_104_om_python_mysql_flask_wtf_2_v1-master/APP_FILMS/genres/gestion_genres_wtf_forms.py
@@ -0,0 +1,27 @@
+"""
+    Fichier : gestion_genres_wtf_forms.py
+    Auteur : OM 2021.03.22
+    Gestion des formulaires avec WTF
+"""
+
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms import SubmitField
+from wtforms.validators import Length
+from wtforms.validators import Regexp
+
+
+class FormWTFAjouterGenres(FlaskForm):
+    """
+        Dans le formulaire "genres_ajouter_wtf.html" on impose que le champ soit rempli.
+        Définition d'un "bouton" submit avec un libellé personnalisé.
+    """
+    nom_materiel_regexp = "^([A-Z]|[a-zÀ-ÖØ-öø-ÿ])[A-Za-zÀ-ÖØ-öø-ÿ]*['\- ]?[A-Za-zÀ-ÖØ-öø-ÿ]+$"
+    nom_materiel_wtf = StringField("Clavioter le materiel ", validators=[Length(min=2, max=20, message="min 2 max 20"),
+                                                                   Regexp(nom_materiel_regexp,
+                                                                          message="Pas de chiffres, de caractères "
+                                                                                  "spéciaux, "
+                                                                                  "d'espace à double, de double "
+                                                                                  "apostrophe, de double trait union")
+                                                                   ])
+    submit = SubmitField("Enregistrer materiel")
